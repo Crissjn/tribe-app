79e65ab8f084b4cd463afcb6abb0359d4b491dd7
Cristiano Craco <cracus89@gmail.com>
2024-11-25 16:10:07 +0100

    first seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,55 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "open-uri"
+
+file = URI.parse("https://t3.ftcdn.net/jpg/06/95/63/36/360_F_695633635_fEruwn09WukVX3T1E8jcTPj7CLte2hPx.jpg").open
+
+puts "Cleaning table..."
+User.destroy_all
+
+kelvin = User.new(
+  username: 'kelvin',
+  email: 'kelvin@gmail.com',
+  password: 'topsecret',
+  password_confirmation: 'topsecret'
+)
+kelvin.photo.attach(io: file, filename: "profile.png", content_type: "image/pgn")
+kelvin.save
+
+james = User.new(
+  username: 'james',
+  email: 'james@gmail.com',
+  password: 'topsecret',
+  password_confirmation: 'topsecret'
+)
+james.photo.attach(io: file, filename: "profile.png", content_type: "image/pgn")
+james.save
+
+rodrigo = User.new(
+  username: 'rodrigo',
+  email: 'rodrigo@gmail.com',
+  password: 'topsecret',
+  password_confirmation: 'topsecret'
+)
+rodrigo.photo.attach(io: file, filename: "profile.png", content_type: "image/pgn")
+rodrigo.save
+
+criss = User.new(
+  username: 'criss',
+  email: 'criss@gmail.com',
+  password: 'topsecret',
+  password_confirmation: 'topsecret'
+)
+criss.photo.attach(io: file, filename: "profile.png", content_type: "image/pgn")
+criss.save
+
+exp1 = Experience.new(
+  max_participants: 5,
+  min_participants: 3,
+  user_id: criss,
+  description: "Open activity Let's walk together somewhere ",
+  exp_type: "Hiking",
+  date:  DateTime.new(2024, 8, 16, 12, 30, 0),
+)
+exp1.save
