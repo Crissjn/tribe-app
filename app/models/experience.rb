9daec931a579411297631de654f6cf3b4d012077
Kelvin Nyingi <adikelvin1@gmail.com>
2024-11-26 17:27:25 +0100

    geocoding

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,6 +2,8 @@ class Experience < ApplicationRecord
   has_one_attached :photo
   has_many :bookings
   belongs_to :user
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
   def full?
      self.bookings == self.max_participants
---
+++ db/migrate/20241126111242_add_coordinates_to_experience.rb
@@ -0,0 +1,6 @@
+class AddCoordinatesToExperience < ActiveRecord::Migration[7.1]
+  def change
+    add_column :experiences, :latitude, :float
+    add_column :experiences, :longitude, :float
+  end
+end
