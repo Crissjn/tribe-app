9110665c3d514fd5a351326c0ffa290989d4c389
Cristiano Craco <cracus89@gmail.com>
2024-12-03 14:35:39 +0100

    all seeds made with blobs..such a pain

--- db/seeds.rb
+++ db/seeds.rb
@@ -304,7 +304,7 @@ exp6 = Experience.new(
   max_participants: 6,
   min_participants: 2,
   user_id: criss.id,
-  location: "Cafe the Minds, Spuistraat 245, 1012 VP Amsterdam",
+  location: "Spuistraat 245, 1012 VP Amsterdam",
   title: "Beeeeers!",
   description: "I discovered this little cafe in the center, probably the last punk cafe in Amsterdam. I want to go check it out!",
   exp_type: "food-drinks",
@@ -349,7 +349,7 @@ exp8 = Experience.new(
   max_participants: 5,
   min_participants: 4,
   user_id: john.id,
-  location: "Hotel & Wellness Zuiver, Koenenkade 8, 1081 KH Amsterdam",
+  location: " Koenenkade 8, 1081 KH Amsterdam",
   title: "A day at the spa!",
   description: "Next week this spa near Amsterdam is having a day with a nice promotion. While in Amsterdam,I would like to take some time to relax but I don't like the idea to go there alone.",
   exp_type: "wellbeing",
@@ -392,7 +392,7 @@ booking = Booking.new(
 booking.save
 puts "experience 9 created"
 
-exp9 = Experience.new(
+exp10 = Experience.new(
   max_participants: 5,
   min_participants: 4,
   user_id: rodrigo.id,
@@ -407,7 +407,7 @@ exp10.photos.attach(drinks_blob)
 exp10.save(validate: false)
 booking = Booking.new(
   user_id: john.id,
-  experience_id: exp9.id
+  experience_id: exp10.id
 )
 booking.save
 booking = Booking.new(
@@ -416,3 +416,28 @@ booking = Booking.new(
 )
 booking.save
 puts "experience 10 created"
+
+exp11 = Experience.new(
+  max_participants: 5,
+  min_participants: 4,
+  user_id: criss.id,
+  location: "Johan Cruijff Boulevard 590, 1101 DS Amsterdam",
+  title: "Heavy Metal concert!",
+  description: "Looking for other fellow metalhead to do some headbanging",
+  exp_type: "culture",
+  price: 0,
+  date: DateTime.new(2024,11,8,18)
+)
+exp11.photos.attach(metal2_blob)
+exp11.save(validate: false)
+booking = Booking.new(
+  user_id: criss.id,
+  experience_id: exp11.id
+)
+booking.save
+booking = Booking.new(
+  user_id: rodrigo.id,
+  experience_id: exp11.id
+)
+booking.save
+puts "experience 11 created"
