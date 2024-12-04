9fbab3b474865026d6c16acacdcd674b2ff93d61
Cristiano Craco <cracus89@gmail.com>
2024-12-04 11:05:12 +0100

    sorry added possibility to add new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_one_attached :photo
-
   has_many :bookings
   has_many :experiences
 
